32dad10f9bbde4e6a0dc9e00e76c6dd4bddf740a
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-05 05:15:36 +0200

    ex10 ok

---
+++ d01/ex10/do_op.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+if ($argc != 4)
+	echo "Incorrect Parameters\n";
+else
+{
+	foreach ($argv as $arg)
+		$op[] = preg_replace('/\\s+/', " ", trim($arg));
+	switch ($op[2])
+	{
+		case "+":
+			echo ($op[1] + $op[3])."\n";
+			break;
+		case "-":
+			echo ($op[1] - $op[3])."\n";
+			break;
+		case "/":
+			echo ($op[1] / $op[3])."\n";
+			break;
+		case "*":
+			echo ($op[1] * $op[3])."\n";
+			break;
+		case "%":
+			echo ($op[1] % $op[3])."\n";
+			break;
+	}
+}
+?>
