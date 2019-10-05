979692813f96ec102f67afa8c4665ab60821e53c
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-05 02:34:30 +0200

    5 OK 3 tjrs pas...

--- d01/ex03/ft_split.php
+++ d01/ex03/ft_split.php
@@ -2,7 +2,10 @@
 <?php
 function ft_split($str)
 {
-	$tab = explode(" ", trim($str));
+	$tab = explode(" ", preg_replace('/\\s+/', " ", trim($str)));
 	return(sort($tab));
 }
+$tab = ft_split("   toooo oooooo       o yu ADFGG m      ljmjm rtyut qsx  fd WWW    ");
+foreach ($tab as $case)
+	echo "$case\n";
 ?>
--- d01/ex03/main.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("ft_split.php");
-
-print_r(ft_split("   y es   aaaa AAA Helo    World   "));
-?>
