aa2bd3387fa05c813d55ae6f21989645887798c9
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-06 00:28:28 +0200

    works

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -2,22 +2,20 @@
 <?php
 function match_up($tab)
 {
-	//print_r($tab);
 	$str = $tab[1].strtoupper($tab[2]).$tab[3];
 	return ($str);
 }
 function match_up2($tab)
 {
-	//print_r($tab);
 	$str = $tab[1].strtoupper($tab[2]).$tab[3].strtoupper($tab[4]).$tab[5];
 	return ($str);
 }
 if ($argc > 1)
 {
 	$file = file_get_contents($argv[1]);
-	$strup = preg_replace_callback("/(<a.*title=\"{0,1})([\\w\\s]+)(\"{0,1}>.*\/a>)/i", "match_up", $file);
-	$strup = preg_replace_callback("/(<a.*?>)(.*)(<.+>)(.*)(<\/a>)/i", "match_up2", $strup);
-	$strup = preg_replace_callback("/(<a.*?>)([\\w\\s]+)(<\/a>)/i", "match_up", $strup);
+	$new = preg_replace_callback("/(<a.*title=\"{0,1})([\\w\\s]+)(\"{0,1}>.*\/a>)/i", "match_up", $file);
+	$new = preg_replace_callback("/(<a.*?>)(.*)(<.*>)(.*)(<\/a>)/i", "match_up2", $new);
+	$new = preg_replace_callback("/(<a.*?>)([\\w\\s]+)(<\/a>)/i", "match_up", $new);
 	echo "$strup\n";
 }
 ?>
