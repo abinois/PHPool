456eb87582b255536ee5d5f60df5c7140300e7ab
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-05 03:54:05 +0200

    07 ok

---
+++ d01/ex08/ft_is_sort.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+function ft_is_sort($tab)
+{
+	$len = count($tab);
+	$old = $tab;
+	sort($tab);
+	while (--$len >= 0)
+		if ($old[$len] != $tab[$len])
+			return (FALSE);
+	return (TRUE);
+}
+?>
