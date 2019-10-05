4a63b19e3ef453cee96d73f380fbde4375f4335e
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-05 06:04:23 +0200

    doop2 ok

---
+++ d01/ex11/do_op2.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+if ($argc != 2)
+	echo "Incorrect Parameters\n";
+else
+{
+	$string = preg_replace('/\\s+/', " ", trim($argv[1]));
+	$tab = sscanf($string, "%d%c%d");
+	if (count($tab) == 3 && is_numeric($tab[0]) && is_numeric($tab[2]))
+	{
+		if ($tab[1] == '+')
+			echo ($tab[0] + $tab[2])."\n";
+		else if ($tab[1] == '-')
+			echo ($tab[0] - $tab[2])."\n";
+		else if ($tab[1] == '*')
+			echo ($tab[0] * $tab[2])."\n";
+		else if ($tab[1] == '/')
+			echo ($tab[0] / $tab[2])."\n";
+		else if ($tab[1] == '%')
+			echo ($tab[0] % $tab[2])."\n";
+	}
+	else
+		echo "Syntax Error\n";
+}
+?>
