1677f25d8b2c1da5a6171e7cce0eb1b90d318831
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-05 23:37:03 +0200

    222

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -2,6 +2,7 @@
 <?php
 function match_up($tab)
 {
+	print_r($tab);
 	$str = $tab[1].strtoupper($tab[2]).$tab[3];
 	return ($str);
 }
@@ -14,7 +15,7 @@ if ($argc > 1)
 {
 	$file = file_get_contents($argv[1]);
 	$strup = preg_replace_callback("/(<a.*title=\"{0,1})(.+)(>.*\/a>)/i", "match_up", $file);
-	$strup = preg_replace_callback("/(<a.*>)(.*)(<.+>)(.*)(<\/a>)/i", "match_up2", $strup);
+	$strup = preg_replace_callback("/(<a.*?>)(.*)(<.+>)(.*)(<\/a>)/i", "match_up2", $strup);
 	$strup = preg_replace_callback("/(<a.*>)(.+)(<\/a>)/ix", "match_up", $strup);
 	echo "$strup\n";
 }
