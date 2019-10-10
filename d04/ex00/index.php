c938f0eaf36ec6123a16930cad6cab13604fa007
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-10 02:08:15 +0200

    ...

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -8,9 +8,9 @@ if ($_GET['submit'] == "OK" && $_GET['login'] != NULL && $_GET['passwd'] != NULL
 ?>
 <html>
 	<body>
-		<form method="get" action="">
+		<form method="get" action="index.php">
 			Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login'];?>" /><br />
-			Mot de passe : <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>" />
+			Mot de passe : <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>" /><br />
 			<input type="submit" name="submit" value="OK" />
 		</form>
 	</body>
