e62585e35525698f4d0b7d74ea51ed9755b88ea9
Arthur BINOIS <abinois@e1r10p8.42.fr>
2019-10-11 16:07:01 +0200

    ex00 OK

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -3,19 +3,33 @@
 Class Color
 {
 	$red, $green, $blue;
-	static $verbose = TRUE;
+	static $verbose = FALSE;
 
 	Function __construct($tab) {
 		if (isset($tab['rgb']))
-			$instance = new Self(array);
+		{
+			$this->red = (intval($tab['rgb']) >> 16) & 0xff;
+			$this->green = (intval($tab['rgb']) >> 8) & 0xff;
+			$this->blue = intval($tab['rgb']) & 0xff;
+			if (Self::$verbose)
+				printf("Color( red: %3d, green: %3d, blue: %3d ) constructed.\n", $this->red, $this->green, $this->blue);
+		}
 		elseif (isset($tab['red']) && isset($tab['green']) && isset($tab['blue']));
-			$instance = new Self();
-		if (Self::$verbose)
-			echo;
+		{
+			$this->red = intval($tab['red']);
+			$this->green = intval($tab['green']);
+			$this->blue = intval($tab['blue']);
+			if (Self::$verbose)
+				printf("Color( red: %3d, green: %3d, blue: %3d ) constructed.\n", $this->red, $this->green, $this->blue);
+		}
 	}
 	Function __destruct() {
+		if (Self::$verbose)
+			printf("Color( red: %3d, green: %3d, blue: %3d ) destructed.\n", $this->red, $this->green, $this->blue);
 	}
 	Function __toString() {
+		return (sprintf("Color( red: %3d, green: %3d, blue: %3d )\n", $this->red, $this->green, $this->blue);
+);
 	}
 	static Function doc() {
 		return (file_get_contents("Color.doc.txt"));
