accc68851e0fc62dfda56e1c5db1ff87b09d7465
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-17 12:28:16 +0200

    cookie ok

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,12 +1,11 @@
-#!/usr/bin/php
 <?php
-if (isset($_GET["action"]) && isset($_GET["name"]))
+if ($_GET["action"] != NULL && $_GET["name"] != NULL && $_GET["name"] != "")
 {
-	if ($_GET["action"] == "set" && isset($_GET["value"]))
+	if ($_GET["action"] === "set" && $_GET["value"] != NULL && $_GET["value"] != "")
 		setcookie($_GET["name"], $_GET["value"], time() + (24 * 60 * 60));
-	elseif ($_GET["action"] == "get" && isset($_COOKIE[$_GET["name"]]))
+	elseif ($_GET["action"] === "get" && $_COOKIE[$_GET["name"]] != NULL)
 		echo $_COOKIE[$_GET["name"]]."\n";
-	elseif ($_GET["action"] == "del" && isset($_COOKIE[$_GET["name"]]))
+	elseif ($_GET["action"] === "del" && $_COOKIE[$_GET["name"]] != NULL)
 		setcookie($_GET["name"], NULL, time() - 1000);
 }
 ?>
