bf94358c58f496f81f2a1865a45ebd4cede13721
Arthur BINOIS <abinois@e2r7p17.42.fr>
2019-10-17 20:31:53 +0200

    detail photos OK

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -2,7 +2,7 @@
 <?php
 if(($source = file_get_contents($argv[1])) === FALSE)
 	exit();
-preg_match_all("/<img.+src=\"(.*?)\"/", $source, $matches);
+preg_match_all("/<img.+src\s*=\s*[\"\'](.*?)[\"\']/", $source, $matches);
 $host = parse_url($argv[1], PHP_URL_HOST);
 if (!file_exists($host))
 	mkdir($host);
