a2a9c640bc73a7838dc08099319a552ba0e33f1c
ARTHUR BINOIS <arthurbinois@MacBook-Pro-de-ARTHUR.local>
2019-10-18 12:26:53 +0200

    ex00 with trimprotection

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 if ($_GET['submit'] == "OK" && $_GET['login'] != NULL && $_GET['passwd'] != NULL)
-{
-	$_SESSION['login'] = $_GET['login'];
-	$_SESSION['passwd'] = $_GET['passwd'];
-}
+	if (trim($_GET['login']) && trim($_GET['passwd']))
+	{
+		$_SESSION['login'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
+	}
 ?>
 <html>
 	<body>
-		<form method="get" action="index.php">
+		<form method="GET" action="index.php">
 			Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login'];?>" /><br />
 			Mot de passe : <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>" /><br />
 			<input type="submit" name="submit" value="OK" />
