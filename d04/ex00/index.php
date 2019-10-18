f9bb4195ad353e64ef28495561d80298bdb4588f
Arthur BINOIS <abinois@e2r7p16.42.fr>
2019-10-18 20:05:00 +0200

    detail de merde

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -12,6 +12,7 @@ if ($_GET['submit'] == "OK" && $_GET['login'] != NULL && $_GET['passwd'] != NULL
 			Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login'];?>" />
 			<br />
 			Mot de passe : <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>" />
+			<br />
 			<input type="submit" name="submit" value="OK" />
 		</form>
 	</body>
