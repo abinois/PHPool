32d8604b169c4b5ceba0f3aef9cba13a79339991
Arthur BINOIS <abinois@e2r6p12.42.fr>
2019-10-21 18:13:04 +0200

    d05 check en cours

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `ft_table`
-{
+(
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
-	`login` VARCHAR(8) NOT NULL DEFAULT `toto`,
-	`groupe` ENUM (`staff`, `student`, `other`) NOT NULL,
-	`date_de_creation` DATE NOT NULL
-};
+	`login` VARCHAR(8) NOT NULL DEFAULT 'toto',
+	`group` ENUM ('staff', 'student', 'other') NOT NULL,
+	`creation_date` DATE NOT NULL
+);
